187cc826d8730e41231bb11dd6eea173c680f05e
Robby Bennett <robby@scitools.com>
2023-11-11 13:45:06 -0700

    add note

--- musicblanket/common.js
+++ musicblanket/common.js
@@ -6,7 +6,7 @@
 ///////////////
 
 
-// Useful for having multiple apps on one site and also OAuth
+// Useful for having multiple apps on one site and also OAuth (Credentials > Authorized redirect URIs)
 const HOME_PATH = '/musicblanket';
 const HOME_URL = `https://robbycbennett.github.io${HOME_PATH}`;
 
